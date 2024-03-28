398198b8b1f08461401ffb5e8b88affd8056c8b1
Karish Gupta <karishgupta2003@gmail.com>
2024-03-28 19:46:41 -0400

    Midi chord function

--- src/scene/chord/ChordScene.cpp
+++ src/scene/chord/ChordScene.cpp
@@ -12,116 +12,36 @@ void ChordScene::init()
   Serial.println("ChordScene init");
 }
 
+void ChordScene::playChord(int buttonPin, int note1, int note2, int note3)
+{
+  if (this->squidbox->getButton(buttonPin)->isPressed())
+  {
+    BLEMidiServer.noteOn(0, note1, 127);
+    BLEMidiServer.noteOn(0, note2, 127);
+    BLEMidiServer.noteOn(0, note3, 127);
+    Serial.println("On");
+    delay(1000);
+    BLEMidiServer.noteOff(0, note1, 127);
+    BLEMidiServer.noteOff(0, note2, 127);
+    BLEMidiServer.noteOff(0, note3, 127);
+    Serial.println("Off");
+  }
+}
+
 void ChordScene::update()
 {
   if (BLEMidiServer.isConnected())
   {
     // If we've got a connection, full velocity (127)
-
-  // A Major Diatonic Chords
-    if (this->squidbox->getButton(1)->isPressed())    
-    {
-      BLEMidiServer.noteOn(0, 93, 127); //A6
-      BLEMidiServer.noteOn(0, 97, 127); //C#7
-      BLEMidiServer.noteOn(0, 100, 127); //E7
-      Serial.println("On");
-      delay(1000);
-      BLEMidiServer.noteOff(0, 93, 127); //A6
-      BLEMidiServer.noteOff(0, 97, 127); //C#7
-      BLEMidiServer.noteOff(0, 100, 127); //E7
-      Serial.println("Off");
-    }
-
-    if (this->squidbox->getButton(2)->isPressed())
-    {
-      
-      BLEMidiServer.noteOn(0, 95, 127); //B6
-      BLEMidiServer.noteOn(0, 98, 127); //D7
-      BLEMidiServer.noteOn(0, 102, 127); //F#7
-      Serial.println("On");
-      delay(1000);
-      BLEMidiServer.noteOff(0, 95, 127); //B6
-      BLEMidiServer.noteOff(0, 98, 127); //D7
-      BLEMidiServer.noteOff(0, 102, 127); //F#7 
-      Serial.println("Off");
-    }
-
-    if (this->squidbox->getButton(3)->isPressed())
-    {
-      BLEMidiServer.noteOn(0, 97, 127); //C#7
-      BLEMidiServer.noteOn(0, 101, 127); //E7
-      BLEMidiServer.noteOn(0, 104, 127); //G#7
-      Serial.println("On");
-      delay(1000);
-      BLEMidiServer.noteOff(0, 97, 127); //C#7
-      BLEMidiServer.noteOff(0, 100, 127); //E7
-      BLEMidiServer.noteOff(0, 104, 127); //G#7
-      Serial.println("Off");
-    }
-
-    if (this->squidbox->getButton(4)->isPressed())
-    {
-      BLEMidiServer.noteOn(0, 98, 127); //D7
-      BLEMidiServer.noteOn(0, 102, 127); //F#7
-      BLEMidiServer.noteOn(0, 105, 127); //A7
-      Serial.println("On");
-      delay(1000);
-      BLEMidiServer.noteOff(0, 98, 127); //D7
-      BLEMidiServer.noteOff(0, 102, 127); //F#7
-      BLEMidiServer.noteOff(0, 105, 127); //A7
-      Serial.println("Off");
-    }
-
-    if (this->squidbox->getButton(5)->isPressed()) { 
-      BLEMidiServer.noteOn(0, 100, 127); //E7
-      BLEMidiServer.noteOn(0, 104, 127); //G#7
-      BLEMidiServer.noteOn(0, 107, 127); //B7
-      Serial.println("On");
-      delay(1000);
-      BLEMidiServer.noteOff(0, 100, 127); //E7
-      BLEMidiServer.noteOff(0, 104, 127); //G#7
-      BLEMidiServer.noteOff(0, 107, 127); //B7
-      Serial.println("Off");
-    }
-
-    if (this->squidbox->getButton(6)->isPressed())
-    {
-      BLEMidiServer.noteOn(0, 101, 127); //F#7
-      BLEMidiServer.noteOn(0, 105, 127); //A7
-      BLEMidiServer.noteOn(0, 108, 127); //C8
-      Serial.println("On");
-      delay(1000);
-      BLEMidiServer.noteOff(0, 102, 127); //F7
-      BLEMidiServer.noteOff(0, 105, 127); //A7
-      BLEMidiServer.noteOff(0, 108, 127); //C8
-      Serial.println("Off");
-    }
-
-    if (this->squidbox->getButton(7)->isPressed())
-    {
-      BLEMidiServer.noteOn(0, 104, 127); //G#7
-      BLEMidiServer.noteOn(0, 107, 127); //B7
-      BLEMidiServer.noteOn(0, 110, 127); //D8
-      Serial.println("On");
-      delay(1000);
-      BLEMidiServer.noteOff(0, 104, 127); //G#7
-      BLEMidiServer.noteOff(0, 107, 127); //B7
-      BLEMidiServer.noteOff(0, 110, 127); //D8
-      Serial.println("Off");
-    }
-
-    if (this->squidbox->getButton(8)->isPressed())
-    {
-      BLEMidiServer.noteOn(0, 105, 127); //A7
-      BLEMidiServer.noteOn(0, 109, 127); //C#8
-      BLEMidiServer.noteOn(0, 112, 127); //E8
-      Serial.println("On");
-      delay(1000);
-      BLEMidiServer.noteOn(0, 105, 127); //A7
-      BLEMidiServer.noteOn(0, 109, 127); //C#8
-      BLEMidiServer.noteOn(0, 112, 127); //E8
-      Serial.println("Off");
-    }
+    // A Major Diatonic Chords
+    playChord(1, 93, 97, 100); //A6, C#7, E7
+    playChord(2, 95, 98, 102); //B6, D7, F#7
+    playChord(3, 97, 101, 104); //C#7, E7, G#7
+    playChord(4, 98, 102, 105); //D7, F#7, A7
+    playChord(5, 100, 104, 107); //E7, G#7, B7
+    playChord(6, 101, 105, 108); //F#7, A7, C8
+    playChord(7, 104, 107, 110); //G#7, B7, D8
+    playChord(8, 105, 109, 112); //A7, C#8, E8
     
   }
 }
