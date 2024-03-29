6c7bc39336f99dacc233c526e5e6f8827a6762be
Karish Gupta <karishgupta2003@gmail.com>
2024-03-29 16:41:17 -0400

    Added midi file and notes have names with MIDI.h

--- src/scene/chord/ChordScene.cpp
+++ src/scene/chord/ChordScene.cpp
@@ -1,6 +1,6 @@
 #include "ChordScene.h"
 #include "Squidbox.h"
-
+#include "util/MIDI.h"
 
 ChordScene::ChordScene(Squidbox *squidbox)
 {
@@ -32,8 +32,6 @@ void ChordScene::playChord(int buttonPin, int note1, int note2, int note3)
     Serial.println("Off " + String(buttonPin) + " " + String(note1) + " " + String(note2) + " " + String(note3));
     squidbox->getButton(buttonPin)->setWasPressed(false);
   }
-
-
 }
 
 void ChordScene::update()
@@ -42,14 +40,13 @@ void ChordScene::update()
   {
     // If we've got a connection, full velocity (127)
     // A Major Diatonic Chords
-    playChord(1, 93, 97, 100); //A6, C#7, E7
-    playChord(2, 95, 98, 102); //B6, D7, F#7
-    playChord(3, 97, 101, 104); //C#7, E7, G#7
-    playChord(4, 98, 102, 105); //D7, F#7, A7
-    playChord(5, 100, 104, 107); //E7, G#7, B7
-    playChord(6, 101, 105, 108); //F#7, A7, C8
-    playChord(7, 104, 107, 110); //G#7, B7, D8
-    playChord(8, 105, 109, 112); //A7, C#8, E8
-    
+    playChord(1, NOTE_A6, NOTE_CSHARP7, NOTE_E7);      // A6, C#7, E7
+    playChord(2, NOTE_B6, NOTE_D7, NOTE_FSHARP7);      // B6, D7, F#7
+    playChord(3, NOTE_CSHARP7, NOTE_E7, NOTE_GSHARP7); // C#7, E7, G#7
+    playChord(4, NOTE_D7, NOTE_FSHARP7, NOTE_A7);      // D7, F#7, A7
+    playChord(5, NOTE_E7, NOTE_GSHARP7, NOTE_B7);      // E7, G#7, B7
+    playChord(6, NOTE_FSHARP7, NOTE_A7, NOTE_C8);      // F#7, A7, C8
+    playChord(7, NOTE_GSHARP7, NOTE_B7, NOTE_D8);      // G#7, B7, D8
+    playChord(8, NOTE_A7, NOTE_CSHARP8, NOTE_E8);      // A7, C#8, E8
   }
 }
