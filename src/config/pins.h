452f7d081a947467dbb093b9ab863a0d6ca32c7d
DennisGarvey <32199720+DennisGarvey@users.noreply.github.com>
2024-04-29 14:49:42 -0400

    rearrange pin definitions for buttons according to layout on pcb

--- src/config/pins.h
+++ src/config/pins.h
@@ -8,11 +8,12 @@
 #define PIN_KNOB_BUTTON -1
 #define PIN_BACK_BUTTON 26
 #define PIN_OK_BUTTON 13
-#define PIN_BUTTON_0 12
-#define PIN_BUTTON_1 27
-#define PIN_BUTTON_2 33
-#define PIN_BUTTON_3 15
-#define PIN_BUTTON_4 32
-#define PIN_BUTTON_5 14
-#define PIN_BUTTON_6 19
-#define PIN_BUTTON_7 21
+#define PIN_BUTTON_0 21
+#define PIN_BUTTON_1 19
+#define PIN_BUTTON_2 12
+#define PIN_BUTTON_3 27
+#define PIN_BUTTON_4 33
+#define PIN_BUTTON_5 15
+#define PIN_BUTTON_6 32
+#define PIN_BUTTON_7 14
+
